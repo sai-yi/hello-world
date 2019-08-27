42dee43a8e4c156014540b3ba5140498f2509715
saiyi <kyakgai@gmail.com>
2019-08-27 19:23:48 +0630

    Test commit to github

---
+++ src/main/java/zero/saiyi/hello_world/App.java
@@ -0,0 +1,13 @@
+package zero.saiyi.hello_world;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
